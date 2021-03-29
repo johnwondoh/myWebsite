65f7965ab9e64d2f5833f5a619aa7e7f3d10b280
John W <johnwondoh@gmail.com>
2021-03-30 00:15:49 +1030

    Updates

---
+++ precache-manifest.6dfda9614c2aee19554e1fecf3e858b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c47b0da52b52c9e9c79d1119d8045ba",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "880f166b832a7b82a376",
+    "url": "/myWebsite/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1ee6cad30492e0adf6bf",
+    "url": "/myWebsite/static/css/main.2038f0ba.chunk.css"
+  },
+  {
+    "revision": "880f166b832a7b82a376",
+    "url": "/myWebsite/static/js/2.9ea7161c.chunk.js"
+  },
+  {
+    "revision": "2c38b520163bc2b832c30f4b552e472d",
+    "url": "/myWebsite/static/js/2.9ea7161c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee6cad30492e0adf6bf",
+    "url": "/myWebsite/static/js/main.24d33a77.chunk.js"
+  },
+  {
+    "revision": "faf76c31ae7a07bbec78",
+    "url": "/myWebsite/static/js/runtime-main.5626a1af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
